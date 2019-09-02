d47d451bd80a91d24c790a0018c91d78d94be0ea
William Neild <wneild5@gmail.com>
2019-09-02 15:59:51 +0100

    Added more options and 'lazy' number formatting

--- src/savedata.js
+++ src/savedata.js
@@ -40,7 +40,7 @@ class Save {
         
         // stuff that isn't user based or needed to be saved across saves
         this.gameData = {
-            version: "0.0.21 ALPHA",
+            version: "0.0.22 ALPHA",
             currentSaveVersion: 2,
             modalOpen: false
         }
