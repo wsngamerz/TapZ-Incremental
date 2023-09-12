dafdd111f348f4e92f145f9f1c71c71a25e21712
William Neild <wneild5@gmail.com>
2023-09-12 23:30:24 +0100

    feat: change colour on death

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -19,12 +19,25 @@
   let zombie: HTMLElement;
   let damageIndicators: HTMLElement;
 
+  const hueClasses = [
+    "hue-rotate-0",
+    "hue-rotate-30",
+    "hue-rotate-90",
+    "hue-rotate-180",
+  ]
+
   $: {
     if (zombie) {
       if ($gameModel.saveData.health <= 0) {
         zombie.classList.add("dead");
+        zombie.classList.remove(...hueClasses);
       } else {
         zombie.classList.remove("dead");
+
+        // add a random hue class to the zombie if there is no hue* class already
+        if (!hueClasses.some(hueClass => zombie.classList.contains(hueClass))) {
+          zombie.classList.add(hueClasses[Math.floor(Math.random() * hueClasses.length)]);
+        }
       }
     }
   }
@@ -79,9 +92,9 @@
   </header>
 
   <main class="flex-grow flex items-center justify-center overflow-hidden">
-
     <div class="transition duration-200 hover:ease-out ease-in hover:scale-110"
-         on:click={clickHandler}>
+         on:click={clickHandler}
+         on:mouseup={e => e.preventDefault()}>
       <div bind:this={zombie} class="zombie"/>
     </div>
   </main>
