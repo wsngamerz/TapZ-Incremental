8f45af80e0988464216caca02c694e840a81d521
William Neild <wneild5@gmail.com>
2023-09-15 13:47:29 +0100

    feat: basic data migration

--- src/lib/save.ts
+++ src/lib/save.ts
@@ -14,6 +14,21 @@ export function save(data: SaveData) {
 	}
 }
 
+function migrate(data: SaveData): SaveData {
+	const blank = new SaveData();
+
+	Object.getOwnPropertyNames(blank).forEach((prop) => {
+		// @ts-ignore
+		if (typeof data[prop] === 'undefined') {
+			console.log(`${prop} was undefined, adding it to saveData`);
+			// @ts-ignore
+			data[prop] = blank[prop];
+		}
+	});
+
+	return data;
+}
+
 export function load(): SaveData {
 	try {
 		// If there is no save data, return a new SaveData object
@@ -21,7 +36,7 @@ export function load(): SaveData {
 
 		// Otherwise, parse the save data and return it
 		const data = localStorage.getItem(saveDataKey);
-		return JSON.parse(<string>data);
+		return migrate(JSON.parse(<string>data));
 	} catch (error) {
 		console.error('Error loading data', error);
 		return new SaveData();
