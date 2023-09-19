7a492297b3157a2b57f0460b1d1558db1d1d44eb
William Neild <wneild5@gmail.com>
2023-09-19 15:55:43 +0100

    feat: implement styling for rarity upgrades

--- src/components/shop-item.svelte
+++ src/components/shop-item.svelte
@@ -22,6 +22,14 @@
   $: level = $gameModel.saveData.upgrades[id].level || 0;
   let cost = upgrade?.getCost() || 0;
 
+  $: glintClass = [
+    "",
+    "rare",
+    "epic",
+    "legendary"
+  // ][level > 0 ? (Math.round(Math.random() * 4)) : 0];
+  ][0];
+
   const buyItem = () => {
     if (canAfford) {
       upgrade?.buy();
@@ -31,24 +39,26 @@
   };
 </script>
 
-<div class="rounded flex gap-2 bg-gray-800 p-2">
-  {#if icon}
-    <div class="block p-2 bg-gray-600 rounded aspect-square shrink-0 grow-0">
-      <svelte:component this={icon} class="w-12 h-12" />
-    </div>
-  {/if}
+<div class={`upgrade-glint ${glintClass}`}>
+  <div class={`rounded flex gap-2 bg-gray-800 p-2 ${level === 0 ? "bg-opacity-70" : ""}`}>
+    {#if icon}
+      <div class="block p-2 bg-gray-600 rounded aspect-square">
+        <svelte:component this={icon} class="w-12 h-12" />
+      </div>
+    {/if}
 
-  <div class="flex-grow text-gray-500 text-sm">
-    <div class="flex justify-between">
-      <h2 class="text-gray-200">{name}</h2>
-      <span>Level: {level}</span>
-    </div>
-    <h3>{extra}</h3>
-    <div class="flex justify-between items-end">
-      <span class={`font-extralight text-gray-200 text-xs px-3 py-0.5 inline-block rounded-full ${badgeVariant}`}>
-        {type}
-      </span>
-      <Button type="yellow" disabled="{!canAfford}" on:click={buyItem}>£{cost}</Button>
+    <div class="flex-grow text-gray-500 text-sm">
+      <div class="flex justify-between">
+        <h2 class="text-gray-200">{name}</h2>
+        <span>Level: {level}</span>
+      </div>
+      <h3>{extra}</h3>
+      <div class="flex justify-between items-end">
+        <span class={`font-extralight text-gray-200 text-xs px-3 py-0.5 inline-block rounded-full ${badgeVariant}`}>
+          {type}
+        </span>
+        <Button type="yellow" disabled="{!canAfford}" on:click={buyItem}>£{cost}</Button>
+      </div>
     </div>
   </div>
 </div>
