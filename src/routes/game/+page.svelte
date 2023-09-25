adddd0285a9d35f6b877b9a1b59eb3f34f3d6580
William Neild <wneild5@gmail.com>
2023-09-25 17:03:23 +0100

    feat: add loading indicator with slight microinteraction delay

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -7,7 +7,10 @@
   import Slideout from "../../components/slideout.svelte";
   import Header from "../../components/header.svelte";
   import Zombie from "../../components/zombie.svelte";
+  import { browser } from "$app/environment";
 
+  let gameWindow: HTMLElement;
+  let loadingIndicator: HTMLElement;
   let damageIndicators: HTMLElement;
 
   const pointerHandler = (event: PointerEvent) => {
@@ -30,11 +33,22 @@
   };
 
   onMount(() => {
+    const start = Date.now();
     startGame();
+    const end = Date.now();
+
+    const duration = end - start;
+    console.log(`Game started in ${duration}ms`);
+    loadingIndicator.remove();
+
+    if (browser) {
+      // Fade in the game window after the game has started with slight delay
+      setTimeout(() => gameWindow.classList.remove("opacity-0"), Math.max(0, 200 - duration));
+    }
   });
 </script>
 
-<div class="relative flex flex-col w-full h-full max-w-4xl xl:max-w-6xl 2xl:max-w-7xl mx-auto">
+<div bind:this={gameWindow} class="game-window opacity-0">
   <Header />
 
   <main class="flex-grow flex items-center justify-center overflow-hidden">
@@ -44,4 +58,10 @@
   <Slideout />
 
   <div class="indicators" bind:this={damageIndicators}></div>
+</div>
+
+<div bind:this={loadingIndicator} class="absolute top-0 left-0 w-screen h-screen flex items-center justify-center">
+  <span class="font-mono font-black text-3xl">
+    Loading...
+  </span>
 </div>
